cad21fb180d05825dfef1789ee59d618597ddeb4
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-04-02 14:37:11 +0200

    Fix ModuleNotFoundException serialVersionUID

--- src/main/java/de/timmi6790/discord_framework/module/exceptions/ModuleNotFoundException.java
+++ src/main/java/de/timmi6790/discord_framework/module/exceptions/ModuleNotFoundException.java
@@ -1,7 +1,7 @@
 package de.timmi6790.discord_framework.module.exceptions;
 
 public class ModuleNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = -4584396945658349823L;
+    private static final long serialVersionUID = -480705207437502908L;
 
     public ModuleNotFoundException(final String module) {
         super("Module " + module + " is not started.");
