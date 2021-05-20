b208009de617317dfc562a7de19f177f284021c7
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-05-20 15:22:49 +0200

    Fix JarModuleProvider
    Experimental fix for the JarModuleProvider. Java 9 broke the old reflection way, the new way requires every addon to be a named module and have no shaded dependencies of the framework.

--- src/main/java/de/timmi6790/discord_framework/module/provider/providers/jar/JarModuleProvider.java
+++ src/main/java/de/timmi6790/discord_framework/module/provider/providers/jar/JarModuleProvider.java
@@ -1,18 +1,21 @@
 package de.timmi6790.discord_framework.module.provider.providers.jar;
 
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import de.timmi6790.discord_framework.module.AbstractModule;
 import de.timmi6790.discord_framework.module.provider.ModuleProvider;
-import de.timmi6790.discord_framework.utilities.ModuleUtilities;
+import io.sentry.Sentry;
 import lombok.extern.log4j.Log4j2;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.module.Configuration;
+import java.lang.module.ModuleFinder;
+import java.lang.module.ModuleReference;
+import java.net.URI;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -29,6 +32,12 @@ public class JarModuleProvider implements ModuleProvider {
             .setPrettyPrinting()
             .create();
 
+    private String getLogName(final ModuleReference moduleReference) {
+        return moduleReference.location()
+                .map(URI::getRawQuery)
+                .orElseGet(() -> moduleReference.descriptor().name());
+    }
+
     @Override
     public String getName() {
         return "Jar";
@@ -42,79 +51,108 @@ public class JarModuleProvider implements ModuleProvider {
         } catch (final IOException ignore) {
         }
 
-        final File[] pluginJars = Module_PATH
-                .toFile()
-                .listFiles((dir, name) -> name.toLowerCase().endsWith(".jar"));
+        try {
+            final ModuleFinder finder = ModuleFinder.of(Module_PATH);
 
-        final Set<Class<? extends AbstractModule>> abstractModules = new HashSet<>();
-        if (pluginJars != null) {
-            for (final File jar : pluginJars) {
-                abstractModules.addAll(this.getAbstractModulesFromJar(jar));
+            final Set<ModuleReference> moduleReferences = finder.findAll();
+            final Set<String> moduleNames = Sets.newHashSetWithExpectedSize(moduleReferences.size());
+            for (final ModuleReference moduleReference : moduleReferences) {
+                moduleNames.add(moduleReference.descriptor().name());
             }
-        }
 
-        return abstractModules;
+            final ModuleLayer parentLayer = ModuleLayer.boot();
+            final Configuration configuration = parentLayer.configuration().resolve(finder, ModuleFinder.of(), moduleNames);
+            final ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+            final ModuleLayer layer = parentLayer.defineModulesWithManyLoaders(configuration, systemClassLoader);
+
+            final Set<Class<? extends AbstractModule>> abstractModules = new HashSet<>();
+            for (final ModuleReference moduleReference : moduleReferences) {
+                abstractModules.addAll(this.getAbstractModulesFromJar(moduleReference, layer));
+            }
+
+            return abstractModules;
+        } catch (final Exception e) {
+            log.catching(e);
+            Sentry.captureException(e);
+            return new HashSet<>();
+        }
     }
 
-    private Optional<Class<?>> getClass(final String path, final URLClassLoader classLoader) {
+    private Optional<Class<?>> getClass(final ModuleReference moduleReference,
+                                        final ModuleLayer moduleLayer,
+                                        final String path) {
         try {
-            final Class<?> clazz = Class.forName(path, true, classLoader);
+            final Class<?> clazz = moduleLayer.findLoader(moduleReference.descriptor().name())
+                    .loadClass(path);
             return Optional.of(clazz);
         } catch (final ClassNotFoundException e) {
             return Optional.empty();
         }
     }
 
-    private List<Class<? extends AbstractModule>> getAbstractModulesFromJar(final File jar) {
-        log.info("Checking {} for Modules.", jar.getName());
+    private Optional<PluginConfig> getPluginConfig(final URL pluginUrl) {
+        if (pluginUrl == null) {
+            return Optional.empty();
+        }
 
-        final List<Class<? extends AbstractModule>> abstractModules = new ArrayList<>();
-        try (final URLClassLoader child = new URLClassLoader(
-                new URL[]{jar.toURI().toURL()},
-                this.getClass().getClassLoader()
-        )) {
-            final URL pluginUrl = child.getResource("plugin.json");
-            if (pluginUrl == null) {
-                log.warn("Can't load {}, no plugins.json found.", jar.getName());
-                return abstractModules;
+        try (final BufferedReader inputStream = new BufferedReader(new InputStreamReader(pluginUrl.openStream(), StandardCharsets.UTF_8))) {
+            return Optional.ofNullable(gson.fromJson(inputStream, PluginConfig.class));
+        } catch (final IOException ignore) {
+            return Optional.empty();
+        }
+    }
+
+    private List<Class<? extends AbstractModule>> getAbstractModulesFromJar(final ModuleReference moduleReference,
+                                                                            final ModuleLayer moduleLayer) {
+        log.info(
+                "Checking {} for Modules.",
+                this.getLogName(moduleReference)
+        );
+
+        final List<Class<? extends AbstractModule>> modules = new ArrayList<>();
+        try {
+            final URL pluginUrl = moduleLayer.findLoader(moduleReference.descriptor().name()).getResource("plugin.json");
+            final Optional<PluginConfig> pluginConfigOpt = this.getPluginConfig(pluginUrl);
+            if (pluginConfigOpt.isEmpty()) {
+                log.warn(
+                        "Can't load {}, no plugins.json found.",
+                        this.getLogName(moduleReference)
+                );
+                return modules;
             }
 
-            ModuleUtilities.addJarToSystemClassLoader(jar);
-
-            try (final BufferedReader inputStream = new BufferedReader(new InputStreamReader(pluginUrl.openStream(), StandardCharsets.UTF_8))) {
-                final PluginConfig plugin = gson.fromJson(inputStream, PluginConfig.class);
-                for (final String path : plugin.getModules()) {
-                    final Optional<Class<?>> pluginClassOpt = this.getClass(path, child);
-                    if (!pluginClassOpt.isPresent()) {
-                        log.warn(
-                                "Can't load Module {} inside {}, unknown path.",
-                                path,
-                                jar.getName()
-                        );
-                        continue;
-                    }
-
-                    if (!AbstractModule.class.isAssignableFrom(pluginClassOpt.get())) {
-                        log.warn(
-                                "Module {} inside {} is not implementing Module!",
-                                path,
-                                jar.getName()
-                        );
-                        continue;
-                    }
-
-                    final Class<? extends AbstractModule> moduleClass = Class.forName(path, true, child)
-                            .asSubclass(AbstractModule.class);
-                    abstractModules.add(moduleClass);
+            final PluginConfig plugin = pluginConfigOpt.get();
+            for (final String path : plugin.getModules()) {
+                final Optional<Class<?>> pluginClassOpt = this.getClass(moduleReference, moduleLayer, path);
+                if (pluginClassOpt.isEmpty()) {
+                    log.warn(
+                            "Can't load Module {} inside {}, unknown path.",
+                            path,
+                            this.getLogName(moduleReference)
+                    );
+                    continue;
                 }
+
+                if (!AbstractModule.class.isAssignableFrom(pluginClassOpt.get())) {
+                    log.warn(
+                            "Module {} inside {} is not implementing Module!",
+                            path,
+                            this.getLogName(moduleReference)
+                    );
+                    continue;
+                }
+
+                final Class<? extends AbstractModule> moduleClass = pluginClassOpt.get()
+                        .asSubclass(AbstractModule.class);
+                modules.add(moduleClass);
             }
         } catch (final Exception e) {
             log.warn(
-                    "Error while trying to load Modules from " + jar.getName() + ".",
+                    "Error while trying to load Modules from " + this.getLogName(moduleReference) + ".",
                     e
             );
         }
 
-        return abstractModules;
+        return modules;
     }
 }
