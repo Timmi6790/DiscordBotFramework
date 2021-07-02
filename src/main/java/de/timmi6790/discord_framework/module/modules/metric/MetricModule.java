891678bc8ab859a1db85708924ff050185ad19f4
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-07-02 10:28:11 +0200

    Optimize discord metrics
    Instead of updating the metrics every x seconds, the metrics are now pulled when required.
    
    - Add application uptime metrics
    - Add discord gateway average ping metric
    - Add discord queued shards metric

--- src/main/java/de/timmi6790/discord_framework/module/modules/metric/MetricModule.java
+++ src/main/java/de/timmi6790/discord_framework/module/modules/metric/MetricModule.java
@@ -2,16 +2,17 @@ package de.timmi6790.discord_framework.module.modules.metric;
 
 import de.timmi6790.discord_framework.module.AbstractModule;
 import io.micrometer.core.instrument.Clock;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
+import io.micrometer.core.instrument.binder.system.UptimeMetrics;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Gauge;
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
 import lombok.EqualsAndHashCode;
@@ -19,28 +20,13 @@ import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
 @EqualsAndHashCode(callSuper = true)
 @Log4j2
 public class MetricModule extends AbstractModule {
-    private static final Gauge DISCORD_GUILDS = Gauge.build()
-            .name("discord_guilds")
-            .help("Discord guilds.")
-            .register();
-
-    private static final Gauge DISCORD_SHARDS_RUNNING = Gauge.build()
-            .name("discord_shards_running")
-            .help("Discord running shards.")
-            .register();
-
     @Getter
     private PrometheusMeterRegistry meterRegistry;
 
     private Undertow metricsServer;
-    private ScheduledFuture<?> executorService;
 
     public MetricModule() {
         super("Metric");
@@ -56,15 +42,40 @@ public class MetricModule extends AbstractModule {
         this.metricsServer.start();
     }
 
-    private void registerJvmMetrics(final MeterRegistry registry) {
+    private void registerMetrics(final MeterRegistry registry) {
+        // Jvm metrics
         new ClassLoaderMetrics().bindTo(registry);
         new JvmMemoryMetrics().bindTo(registry);
         new ProcessorMetrics().bindTo(registry);
         new JvmThreadMetrics().bindTo(registry);
+        new UptimeMetrics().bindTo(registry);
 
         try (final JvmGcMetrics gcMetrics = new JvmGcMetrics()) {
             gcMetrics.bindTo(registry);
         }
+
+        // Discord metrics
+        final ShardManager discord = this.getDiscord();
+        Gauge
+                .builder("discord.guilds", discord, d -> d.getGuilds().size())
+                .description("discord guild count")
+                .register(registry);
+
+        Gauge
+                .builder("discord.shards", discord, ShardManager::getShardsRunning)
+                .tags("state", "running")
+                .description("discord shards")
+                .register(registry);
+        Gauge
+                .builder("discord.shards", discord, ShardManager::getShardsQueued)
+                .tags("state", "queued")
+                .description("discord shards")
+                .register(registry);
+
+        Gauge
+                .builder("discord.gateway.ping", discord, ShardManager::getAverageGatewayPing)
+                .description("discord average gateway ping")
+                .register(registry);
     }
 
     @Override
@@ -76,28 +87,13 @@ public class MetricModule extends AbstractModule {
         );
 
         this.startMetricsServer();
-        this.registerJvmMetrics(this.meterRegistry);
-
-        return true;
-    }
-
-    @Override
-    public boolean onEnable() {
-        this.executorService = Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
-                () -> {
-                    final ShardManager discord = this.getDiscord();
-                    DISCORD_GUILDS.set(discord.getGuilds().size());
-                    DISCORD_SHARDS_RUNNING.set(discord.getShardsRunning());
-                },
-                0, 10, TimeUnit.SECONDS
-        );
+        this.registerMetrics(this.meterRegistry);
 
         return true;
     }
 
     @Override
     public boolean onDisable() {
-        this.executorService.cancel(true);
         this.metricsServer.stop();
         return true;
     }
