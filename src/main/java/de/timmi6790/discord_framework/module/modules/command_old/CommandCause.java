90573520fa0c664fa0ea925ca2abed31e0de29d4
Tim <13711365+Timmi6790@users.noreply.github.com>
2021-07-09 11:48:58 +0200

    Remove old command module

--- src/main/java/de/timmi6790/discord_framework/module/modules/command_old/CommandCause.java
+++
@@ -1,6 +0,0 @@
-package de.timmi6790.discord_framework.module.modules.command_old;
-
-public enum CommandCause {
-    USER,
-    EMOTES
-}
--- src/main/java/de/timmi6790/discord_framework/module/modules/command_old/Config.java
+++
@@ -1,9 +0,0 @@
-package de.timmi6790.discord_framework.module.modules.command_old;
-
-import lombok.Data;
-
-@Data
-public class Config {
-    private String mainCommand = "stat ";
-    private boolean setDiscordActivity = true;
-}
