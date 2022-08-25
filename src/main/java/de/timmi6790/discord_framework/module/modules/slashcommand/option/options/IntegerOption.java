bb8b824bccb766241f74dde9100d9a67227f0414
Tim <13711365+Timmi6790@users.noreply.github.com>
2022-08-25 17:41:31 +0200

    Improve IntegerOption

--- src/main/java/de/timmi6790/discord_framework/module/modules/slashcommand/option/options/IntegerOption.java
+++ src/main/java/de/timmi6790/discord_framework/module/modules/slashcommand/option/options/IntegerOption.java
@@ -3,10 +3,14 @@ package de.timmi6790.discord_framework.module.modules.slashcommand.option.option
 import de.timmi6790.discord_framework.module.modules.slashcommand.option.Option;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.util.Optional;
 
 public class IntegerOption extends Option<Integer> {
+    private Integer min = null;
+    private Integer max = null;
+
     public IntegerOption(final String name, final String description) {
         super(name, description, OptionType.INTEGER);
     }
@@ -19,5 +23,28 @@ public class IntegerOption extends Option<Integer> {
     @Override
     public Optional<Integer> convertValue(final OptionMapping mapping) {
         return Optional.of(mapping.getAsInt());
+    }
+
+    @Override
+    public OptionData build() {
+        final OptionData optionData = super.build();
+
+        if (this.min != null) {
+            optionData.setMinValue(this.min);
+        }
+
+        if (this.max != null) {
+            optionData.setMaxValue(this.max);
+        }
+
+        return optionData;
+    }
+
+    public void setMax(final int max) {
+        this.max = max;
+    }
+
+    public void setMin(final int min) {
+        this.min = min;
     }
 }
