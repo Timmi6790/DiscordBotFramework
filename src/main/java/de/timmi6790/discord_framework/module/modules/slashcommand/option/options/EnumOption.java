3784bfe10292e07ad0e56705377ee5ae565e22f2
Tim <13711365+Timmi6790@users.noreply.github.com>
2022-08-25 17:51:34 +0200

    Add EnumOption

---
+++ src/main/java/de/timmi6790/discord_framework/module/modules/slashcommand/option/options/EnumOption.java
@@ -0,0 +1,34 @@
+package de.timmi6790.discord_framework.module.modules.slashcommand.option.options;
+
+import de.timmi6790.discord_framework.module.modules.slashcommand.option.Option;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+import java.util.Optional;
+
+public class EnumOption<T extends Enum<T>> extends Option<T> {
+    private final Class<T> enumClass;
+
+    public EnumOption(final Class<T> enumClass, final String name, final String description) {
+        super(name, description, OptionType.STRING);
+
+        this.enumClass = enumClass;
+
+        this.addTypeOptions(enumClass.getEnumConstants());
+    }
+
+    @Override
+    public String convertToOption(final T option) {
+        return option.name();
+    }
+
+    @Override
+    public Optional<T> convertValue(final OptionMapping mapping) {
+        try {
+            final T value = Enum.valueOf(this.enumClass, mapping.getAsString());
+            return Optional.of(value);
+        } catch (final Exception exception) {
+            return Optional.empty();
+        }
+    }
+}
